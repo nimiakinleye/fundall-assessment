4bd8559540548cd194cea1d57086fee34702b3ee
DESKTOP-K9BCO3M\USER <toluwanimiakinleye01@gmail.com>
2022-04-01 10:50:24 +0100

    added daily summary

--- src/components/Dashboard/DailyExpensesSummary.vue
+++ src/components/Dashboard/DailyExpensesSummary.vue
@@ -1,3 +1,28 @@
+<script>
+export default {
+  data () {
+    return {
+      data: decodeURIComponent(localStorage.getItem('dailyExpenses'))
+    }
+  },
+  computed: {
+    dailyExpenses(){
+      return localStorage.getItem('dailyExpenses') ? JSON.parse(decodeURIComponent(localStorage.getItem('dailyExpenses'))) : false
+    }
+  },
+}
+</script>
+
 <template>
-  <div class="fundall-black">Daily Expenses Summary</div>
+  <div class="fundall-black lg:w-96 h-96 p-8 rounded-xl bg-white">
+    <h1>Daily Expenses Summary</h1>
+    <div class="flex justify-between mt-8">
+      <div>{{ dailyExpenses.date }}</div>
+      <div>{{ dailyExpenses.value }}</div>
+    </div>
+    <div v-if="!dailyExpenses" class="flex flex-col h-full font-semibold justify-center items-center">
+      <img class="w-40 mb-8" src="/src/assets/empty_cart.svg" alt="">
+      <p>No expenses recorded today</p>
+    </div>
+  </div>
 </template>
--- src/components/Dashboard/TodayExpenses.vue
+++ src/components/Dashboard/TodayExpenses.vue
@@ -1,17 +1,81 @@
 <script>
 import CustomInput from "../reusable/Input.vue";
+import Button from "../reusable/Button.vue";
 export default {
   components: {
     CustomInput,
+    Button,
+  },
+  data() {
+    return {
+      value1: null,
+      value2: null,
+      value3: null,
+    };
+  },
+  computed: {
+    todayExpenseTotal() {
+      return Number(this.value1) + Number(this.value2) + Number(this.value3);
+    },
+    withDate() {
+      var data = {
+        value: this.todayExpenseTotal,
+        date: new Date().toDateString(),
+      };
+      return data;
+    },
+  },
+  methods: {
+    submit() {
+      if (this.todayExpenseTotal === 0) {
+        return
+      }
+      // localStorage.setItem('dailyExpenses', this.withDate);
+      // console.log(this.withDate);
+      localStorage.setItem(
+        "dailyExpenses",
+        encodeURIComponent(JSON.stringify(this.withDate))
+      );
+      window.location.href = "/dashboard";
+    },
   },
 };
 </script>
 <template>
   <div class="mt-8">
     <p class="-mb-4 fundall-black">Today's Expenses</p>
-    <div class="sm:grid grid-cols-2 gap-x-12">
-      <CustomInput placeholder="Enter an expense value" />
-      <CustomInput placeholder="Enter an expense value" />
+    <div>
+      <form @submit.prevent="submit">
+        <div class="sm:grid grid-cols-2 gap-x-12">
+          <CustomInput placeholder="Enter an expense name" />
+          <CustomInput
+            type="number"
+            v-model="value1"
+            placeholder="Enter expense value"
+          />
+          <CustomInput placeholder="Enter an expense name" />
+          <CustomInput
+            type="number"
+            v-model="value2"
+            placeholder="Enter expense value"
+          />
+          <CustomInput placeholder="Enter an expense name" />
+          <CustomInput
+            type="number"
+            v-model="value3"
+            placeholder="Enter expense value"
+          />
+        </div>
+        <div class="flex justify-end items-center mt-6">
+          <p>Total Actual Expenses: ₦</p>
+          <span class="p-2 bg-white rounded ml-1 fundall-black">{{
+            todayExpenseTotal
+          }}</span>
+        </div>
+        <div class="mt-8 flex justify-center">
+          <Button buttonTitle="save Today's expenses" />
+        </div>
+      </form>
     </div>
   </div>
 </template>
